7e54af287c873fc5ce8f75c3202b248c4b80e7b5
Salih <salihpdm.0036@gmail.com>
2019-06-16 22:43:32 +0530

    Bug fix: Animation fps 	Author: Farzeen

--- src/js/fog/init.js
+++ src/js/fog/init.js
@@ -72,8 +72,12 @@ const render = () => {
 
 };
 const renderLoop = () => {
-  render();
-  requestAnimationFrame(renderLoop);
+  if(window.pageYOffset < 100) {
+      render();
+      requestAnimationFrame(renderLoop);
+  } else {
+      window.setTimeout(renderLoop, 1000);
+  }
 };
 const resizeCamera = () => {
   camera.aspect = resolution.x / resolution.y;
@@ -84,6 +88,7 @@ const resizeWindow = () => {
   canvas.width = resolution.x;
   canvas.height = resolution.y;
   resizeCamera();
+  renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(resolution.x, resolution.y);
 };
 const on = () => {
@@ -117,7 +122,6 @@ const init = () => {
     clock.start();
 
     document.addEventListener('mousemove', mouseMove, false);
-    document.addEventListener('touchmove', mouseMove, false);
     loader.load('./font/Lato.json', (font) => {
       nodeText.createObj(font);
 
